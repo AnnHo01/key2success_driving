b6891b21c7f2ecd009dc6c764e2adda8ad956033
aho1304 <aho@artisreit.com>
2023-08-28 16:56:07 -0500

    Fix submission approval form

--- src/app/reviewHandler/page.js
+++ src/app/reviewHandler/page.js
@@ -55,37 +55,9 @@ function Home() {
                 <Container>
                     <Row>
                         <Col className='mb-3 mb-lg-0 headline'>
-                            { method == 'approve' && submission != undefined ? (
+                            { method == 'approve' ? (
                                     <>
                                         <h1>We are processing your approval...</h1>
-                                        <section style={{height: 0, overflow: 'hidden', padding: 0}}>
-                                            <form name="approved_exp_form" method="POST" netlify onSubmit={handleSubmit}>
-                                                <input type="hidden" name="form-name" value="approved_exp_form" />
-                                                <Container>
-                                                <Row className='d-flex justify-content-center' style={{gap: '40px'}}>
-                                                    <Col xs={12} lg={8}>
-                                                    <label htmlFor='fname'>Name*</label>
-                                                    <input type='text' id='fname' name='fname' placeholder='Type Your Name' defaultValue={submission.fname} required />
-                                                    </Col>
-                                                    <Col xs={12} lg={8}>
-                                                    <label htmlFor='email'>Email</label>
-                                                    <input type='email' id='email' name='email' placeholder='Type Your Email' defaultValue={submission.email} />
-                                                    </Col>
-                                                    <Col xs={12} lg={8}>
-                                                    <label htmlFor='pnum'>Phone Number</label>
-                                                    <input type='number' id='pnum' name='pnum' placeholder='Type Your Phone' defaultValue={submission.pnum} />
-                                                    </Col>
-                                                    <Col xs={12} lg={8}>
-                                                    <label htmlFor='fname'>Your Experience</label>
-                                                    <textarea id='experience' name='experience' rows={4} placeholder='Tell Us What You Think' defaultValue={submission.experience}></textarea>
-                                                    </Col>
-                                                    <Col xs={12} lg={8} className='d-flex justify-content-center'>
-                                                    <button type="submit" className='call_btn'>Send Feedback</button>
-                                                    </Col>
-                                                </Row>
-                                                </Container>
-                                            </form>
-                                        </section>
                                     </>
                                 )
                                  : (
@@ -97,6 +69,36 @@ function Home() {
                 </Container>
                 </div>
             </header>
+            <main>
+                <section style={{height: 0, overflow: 'hidden', padding: 0}}>
+                    <form name="approved_exp_form" method="POST" netlify onSubmit={handleSubmit}>
+                        <input type="hidden" name="form-name" value="approved_exp_form" />
+                        <Container>
+                        <Row className='d-flex justify-content-center' style={{gap: '40px'}}>
+                            <Col xs={12} lg={8}>
+                            <label htmlFor='fname'>Name*</label>
+                            <input type='text' id='fname' name='fname' placeholder='Type Your Name' defaultValue={submission != undefined && submission.fname} required />
+                            </Col>
+                            <Col xs={12} lg={8}>
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' id='email' name='email' placeholder='Type Your Email' defaultValue={submission != undefined && submission.email} />
+                            </Col>
+                            <Col xs={12} lg={8}>
+                            <label htmlFor='pnum'>Phone Number</label>
+                            <input type='number' id='pnum' name='pnum' placeholder='Type Your Phone' defaultValue={submission != undefined && submission.pnum} />
+                            </Col>
+                            <Col xs={12} lg={8}>
+                            <label htmlFor='fname'>Your Experience</label>
+                            <textarea id='experience' name='experience' rows={4} placeholder='Tell Us What You Think' defaultValue={submission != undefined && submission.experience}></textarea>
+                            </Col>
+                            <Col xs={12} lg={8} className='d-flex justify-content-center'>
+                            <button type="submit" className='call_btn'>Send Feedback</button>
+                            </Col>
+                        </Row>
+                        </Container>
+                    </form>
+                </section>
+            </main>
         </>
     );
 }
