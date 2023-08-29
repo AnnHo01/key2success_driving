ad91d3596e686493678f302b1a9d28841cf936c1
aho1304 <aho@artisreit.com>
2023-08-29 09:14:35 -0500

    Hide access token

--- src/app/components/fetchContext.js
+++ src/app/components/fetchContext.js
@@ -1,9 +1,8 @@
 const apiUrl = 'https://api.netlify.com/api/v1/';
-// const token = process.env.NETLIFY_ACCESS_TOKEN;
-const token = 'UwpTiKClDp56Sgb5tPLuUQMgztlCDSEktiFXAHxe8Ho';
+const token = process.env.NETLIFY_ACCESS_TOKEN;
 
 export function getReviews(){
-    return fetch(apiUrl + 'forms/64eb7278a7b0640008c67b9e/submissions', {
+    return fetch(apiUrl + `forms/${process.env.FORM_ID}/submissions`, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
